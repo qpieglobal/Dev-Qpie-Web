5e774ea366daae179a593702b88d839fee747f0e
Abhishek Kedar <abhishekkedar246@hotmail.com>
2023-08-21 15:52:02 +0530

    removed creds

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,15 +8,7 @@ import { _window } from "./window";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBm7gFHReIJ29A0mKxjLDX6ArcBgwdsAWo",
-  authDomain: "qpie-f342e.firebaseapp.com",
-  projectId: "qpie-f342e",
-  storageBucket: "qpie-f342e.appspot.com",
-  messagingSenderId: "610588246781",
-  appId: "1:610588246781:web:f11730fc48cd6b7ef07494",
-  measurementId: "G-5NCCVS01TE",
-};
+const firebaseConfig = {};
 
 let firebaseApp =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
